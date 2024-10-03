f72eec7062f7c40f8fd31c0bb9a2d12f152e947d
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-10-03 19:16:45 -0300

    agregando estilos al form

--- src/app/components/CartForm/CartForm.js
+++ src/app/components/CartForm/CartForm.js
@@ -28,36 +28,65 @@ const response = await axios.post('/api/add-contact', data);
 
   return (
     <div>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={async (values) => {
-      addContact(values)
-        }}
+    <Formik
+  initialValues={{
+    firstName: "",
+    lastName: "",
+    email: "",
+  }}
+  onSubmit={async (values) => {
+    addContact(values);
+  }}
+>
+  {({ isSubmitting, errors, touched }) => (
+    <Form className="flex flex-col space-y-4">
+      <label htmlFor="firstName" className="text-pink-500 text-sm font-semibold">
+        First Name
+      </label>
+      <Field
+        name="firstName"
+        placeholder="Jane"
+        validate={firstName}
+        className="border-2 border-pink-500 bg-pink-200 text-white p-2 rounded-md placeholder-white font-inter font-medium "
+      />
+      {errors.firstName && touched.firstName && (
+        <p className="text-yellow-400 text-sm">Completar</p>
+      )}
+
+      <label htmlFor="lastName" className="text-pink-500 text-sm font-semibold">
+        Last Name
+      </label>
+      <Field
+        name="lastName"
+        placeholder="Doe"
+        className="border-2 border-pink-500 bg-pink-200 text-white p-2 rounded-md placeholder-white"
+      />
+
+      <label htmlFor="email" className="text-pink-500 text-sm font-semibold">
+        Email
+      </label>
+      <Field
+        name="email"
+        placeholder="jane@acme.com"
+        type="email"
+        validate={validateEmail}
+        className="border-2 border-pink-500 bg-pink-200 text-white p-2 rounded-md placeholder-white"
+      />
+      {errors.email && touched.email && (
+        <p className="text-yellow-400 text-sm">Completar</p>
+      )}
+
+      <button
+        type="submit"
+        disabled={isSubmitting}
+        className="border-4 border-yellow-400 bg-pink-500 text-white p-2 rounded-md hover:bg-pink-600 disabled:bg-gray-400"
       >
-        {({ isSubmitting, errors,touched }) => (
-          <Form>
-            <label htmlFor='firstName'>First Name</label>
-            <Field name='firstName' placeholder='Jane' validate={firstName} />
-          {errors.firstName && touched.firstName && <p>Completar</p>}
+        Submit
+      </button>
+    </Form>
+  )}
+</Formik>
 
-
-            <label htmlFor='lastName'>Last Name</label>
-            <Field name='lastName' placeholder='Doe' />
-
-            <label htmlFor='email'>Email</label>
-            <Field name='email' placeholder='jane@acme.com' type='email' validate={validateEmail} />
-            {errors.email && touched.email && <p>Completar</p>}
-
-            <button type='submit' disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
     </div>
   );
 };
