3ba8de45fb089f85755434d01ddfc74be09ae1fb
assolucionesmultimediales <multimediales.solutions@gmail.com>
2024-10-03 23:07:45 -0300

    se agrego link en la imagen de la card que lleva a la page id

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,18 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 const ProductCard = ({ item, key }) => {
   return (
     <div key={key} className='col-span-3 flex justify-center items-center flex-col'>
-      <Image
-      className='border-4 border-yellow-400 '
-        src={`/imgs/${item.image}`}
-        alt={item.name}
-        width={0}
-        height={0}
-        sizes='100vw'
-        style={{ width: '85%', height: 'auto' }}
-      />
+      <Link href={`/product/${item._id}`}>
+        <Image
+          className='border-4 border-yellow-400'
+          src={`/imgs/${item.image}`}
+          alt={item.name}
+          width={0}
+          height={0}
+          sizes='100vw'
+          style={{ width: '85%', height: 'auto' }}
+        />
+      </Link>
       <h2 className='text-pink-500 text-lg font-inter font-bold mt-4'>{item.name}</h2>
       <h2 className='text-pink-500 text-l font-medium mt-2 mb-2'>{item.author}</h2>
       <Link className='text-yellow-400 font-bold mb-12 font-montserrat' href={`/product/${item._id}`}>Ver más</Link>
